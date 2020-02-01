f51e64d76f5da63f9be77a0661fa4cc9ee4ded6d
UnicornsOnLSD <jmsharvey771@gmail.com>
2020-02-01 14:22:08 +0000

    Finished basic website

--- website/lib/main.dart
+++ website/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:website/github_icon_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,15 +12,30 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Text(
                 "James Harvey",
                 style: TextStyle(fontSize: 72),
               ),
-              RaisedButton.icon(
-                icon: SvgPicture.asset("assets/github.svg"),
-                label: Text("Github"),
-                onPressed: () => launch("https://www.github.com/UnicornsOnLSD"),
+              ButtonBar(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  RaisedButton.icon(
+                    icon: Icon(GithubIcon.github),
+                    label: Text("Github"),
+                    onPressed: () =>
+                        launch("https://www.github.com/UnicornsOnLSD"),
+                  ),
+                  RaisedButton.icon(
+                    icon: Icon(Icons.email),
+                    label: Text("Email"),
+                    onPressed: () => launch("mailto:jmsharvey771@gmail.com"),
+                  )
+                ],
               )
             ],
           ),
